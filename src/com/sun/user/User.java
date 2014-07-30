f4a8165452c09ee00b6d2cefca7694dc39c561a6
neihcop <neihcop@gmail.com>
2014-07-30 23:52:13 +0800

    update add hibernate -begin

---
+++ src/com/sun/user/User.java
@@ -0,0 +1,51 @@
+package com.sun.user;
+
+import java.sql.Date;
+
+public class User {
+	
+	private String id;
+	private String name;
+	private String email;
+	private String mobile;
+	private Date createtime;
+	private Date modifytime;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public Date getCreatetime() {
+		return createtime;
+	}
+	public void setCreatetime(Date createtime) {
+		this.createtime = createtime;
+	}
+	public Date getModifytime() {
+		return modifytime;
+	}
+	public void setModifytime(Date modifytime) {
+		this.modifytime = modifytime;
+	}
+
+}
