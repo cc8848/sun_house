adbdc8d1751f14deafb401512397b18403a6e83b
neihcop <neihcop@gmail.com>
2014-07-31 23:27:48 +0800

    update date

--- src/com/drsun/test/testHibernate3.java
+++ src/com/drsun/test/testHibernate3.java
@@ -1,6 +1,6 @@
 package com.drsun.test;
 
-import java.sql.Date;
+
 import com.drsun.model.User;
 import com.drsun.model.UserDAO;
 import com.drsun.model.UserHibernateDAO;
@@ -10,13 +10,14 @@ public class testHibernate3 {
 	public static void main(String[] args) {
 		
 		 UserDAO userDAO = new UserHibernateDAO();
-		 java.util.Date now = new java.util.Date();
+		
 		 User user=new User();
 		 
 		 user.setName("ด๚ธี");
 		 user.setEmail("poop@");
-		
+		 java.util.Date now = new java.util.Date();
 		 user.setCreatetime(new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(now));
+		 
 		 userDAO.save(user);
 
 	}
